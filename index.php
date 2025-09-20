3ab7d6c3130591eb168480fc789ccbb51c69098b
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2025-09-20 18:53:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+    //Redireciona para a listagem de alunos
+    header("Location: /lpw/TrabalhoDaniel2025-main/CRUD_em_PHP_com_arquitetura_MVC-main/LPW-Gestao_De_Computadores/view/ordem_servico/listar.php");
+
