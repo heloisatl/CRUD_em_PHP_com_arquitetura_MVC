47aaa3c51323c66023da8c5c364619a7a30ec7fb
Heloisa T. L. <134729931+heloisatl@users.noreply.github.com>
2025-09-20 18:52:03 -0300

    Delete CRUD_em_PHP_com_arquitetura_MVC-main directory

--- CRUD_em_PHP_com_arquitetura_MVC-main/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-    //Redireciona para a listagem de alunos
-    header("Location: /lpw/TrabalhoDaniel2025-main/CRUD_em_PHP_com_arquitetura_MVC-main/LPW-Gestao_De_Computadores/view/ordem_servico/listar.php");
-
